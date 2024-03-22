df13033d9e5f605a86255340133c8c3ad17dce42
Andrew Acin <andrew_acin@hotmail.com>
2024-03-22 14:06:05 -0700

    second commit

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
 class Casino {
-    // Write code here
-};
+    constructor(name, isFakeCoin = false, timesPlayed = 0) {
+        this.name = name;
+        this.timesPlayed = timesPlayed;
+        this.isFakeCoin = isFakeCoin
+    }
+
+    playGame(betAmount) {
+        let result;
+        if (this.isFakeCoin) {// When isFakeCoin is true casino wins
+            result = true;
+        } else {
+            result = Math.random() >= 0.5;
+        }
+        this.timesPlayed++;
+        if (result) {
+            console.log('Casino wins');
+            return false;
+        } else {
+            console.log(`Player wins bet amount ${betAmount}`);
+            return true;
+        }
+        
+    }
+
+}
+
 
 // TESTS
 const myCasino = new Casino("HackerU Casino");
@@ -10,15 +34,16 @@ myCasino.playGame(15);
 myCasino.playGame(25);
 myCasino.playGame(35);
 
+
 // BONUS TESTS
-/*
+
 const myBonusCasino = new Casino("HackerU Bonus Casino", true);
 console.log(myBonusCasino);
 myBonusCasino.playGame(5);
 myBonusCasino.playGame(15);
 myBonusCasino.playGame(25);
 myBonusCasino.playGame(35);
-*/
+
 
 // Extra BONUS TESTS
 /*
